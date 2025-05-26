cdf3e713b35ff6f4378b525f95a143007a546f12
David Wallace Croft <david@CroftSoft.com>
2025-05-26 16:47:07 -0400

    numbers.txt

--- src/bin/ch05-p091-why/main.rs
+++ src/bin/ch05-p091-why/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, Wold!");
+#![feature(coroutines)]
+#![feature(coroutine_trait)]
+
+use ::rand::prelude::*;
+use ::std::fs::{File, OpenOptions};
+use ::std::io::{self, Write};
+use ::std::ops::{Coroutine, CoroutineState};
+use ::std::pin::Pin;
+use ::std::time::Instant;
+
+fn main() -> io::Result<()> {
+  let mut rng: ThreadRng = rand::rng();
+
+  let numbers: Vec<i32> = (0..200_000).map(|_| rng.random()).collect();
+
+  let start = Instant::now();
+
+  for &number in &numbers {
+    if let Err(e) = append_number_to_file(number) {
+      eprintln!("Failed to write to file: {}", e);
+    }
+  }
+
+  let duration = start.elapsed();
+
+  println!("Time elapsed in file operations is: {:?}", duration);
+
+  Ok(())
+}
+
+fn append_number_to_file(n: i32) -> io::Result<()> {
+  let mut file = OpenOptions::new()
+    .create(true)
+    .append(true)
+    .open("numbers.txt")?;
+
+  writeln!(file, "{}", n)?;
+
+  Ok(())
 }
