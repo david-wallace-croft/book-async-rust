f0122c3cdb6aae7de7e8ff0a3c25a2975c5549f2
David Wallace Croft <david@CroftSoft.com>
2025-07-03 07:27:28 -0500

    method implementations

--- src/ch10/executor/mod.rs
+++ src/ch10/executor/mod.rs
@@ -4,7 +4,10 @@ use ::std::{
   collections::VecDeque,
   future::Future,
   pin::Pin,
-  sync::{Arc, mpsc::Receiver},
+  sync::{
+    Arc,
+    mpsc::{self, Receiver},
+  },
   task::{Context, Poll, Waker},
 };
 
@@ -24,7 +27,21 @@ impl Executor {
   }
 
   pub fn poll(&mut self) {
-    todo!()
+    let mut task: Task = match self.polling.pop_front() {
+      Some(task) => task,
+      None => return,
+    };
+
+    let waker: Arc<Waker> = task.waker.clone();
+
+    let context: &mut Context<'_> = &mut Context::from_waker(&waker);
+
+    match task.future.as_mut().poll(context) {
+      Poll::Ready(()) => {},
+      Poll::Pending => {
+        self.polling.push_back(task);
+      },
+    }
   }
 
   pub fn spawn<F, T>(
@@ -35,6 +52,22 @@ impl Executor {
     F: Future<Output = T> + 'static + Send,
     T: Send + 'static,
   {
-    todo!()
+    let (tx, rx) = mpsc::channel();
+
+    let future: Pin<Box<dyn Future<Output = ()> + Send>> =
+      Box::pin(async move {
+        let result: T = future.await;
+
+        let _ = tx.send(result);
+      });
+
+    let task: Task = Task {
+      future,
+      waker: self.create_waker(),
+    };
+
+    self.polling.push_back(task);
+
+    rx
   }
 }
