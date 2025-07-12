0472bafe54d9d55b44eb6af8a15caee9d66048cb
David Wallace Croft <david@CroftSoft.com>
2025-07-12 10:33:46 -0500

    multi-threaded

--- src/ch11_p227_testing/mod.rs
+++ src/ch11_p227_testing/mod.rs
@@ -2,12 +2,20 @@
 
 use ::std::sync::atomic::{AtomicUsize, Ordering};
 
-static COUNTER: AtomicUsize = AtomicUsize::new(0);
+static COUNTER_MULTI_THREADED: AtomicUsize = AtomicUsize::new(0);
 
-async fn unsafe_add() {
-  let value: usize = COUNTER.load(Ordering::SeqCst);
+static COUNTER_SINGLE_THREADED: AtomicUsize = AtomicUsize::new(0);
 
-  COUNTER.store(value + 1, Ordering::SeqCst);
+async fn unsafe_add_single_threaded() {
+  let value: usize = COUNTER_SINGLE_THREADED.load(Ordering::SeqCst);
+
+  COUNTER_SINGLE_THREADED.store(value + 1, Ordering::SeqCst);
+}
+
+async fn unsafe_add_multi_threaded() {
+  let value: usize = COUNTER_MULTI_THREADED.load(Ordering::SeqCst);
+
+  COUNTER_MULTI_THREADED.store(value + 1, Ordering::SeqCst);
 }
 
 #[cfg(test)]
@@ -17,7 +25,7 @@ mod tests {
   use ::tokio::task::JoinHandle;
 
   #[test]
-  fn test_data_race() {
+  fn test_data_race_single_threaded() {
     let runtime: Runtime =
       Builder::new_current_thread().enable_all().build().unwrap();
 
@@ -26,7 +34,7 @@ mod tests {
     let total: usize = 100_000;
 
     for _ in 0..total {
-      let handle: JoinHandle<()> = runtime.spawn(unsafe_add());
+      let handle: JoinHandle<()> = runtime.spawn(unsafe_add_single_threaded());
 
       handles.push(handle);
     }
@@ -36,9 +44,34 @@ mod tests {
     }
 
     assert_eq!(
-      COUNTER.load(Ordering::SeqCst),
+      COUNTER_SINGLE_THREADED.load(Ordering::SeqCst),
       total,
       "race condition occurred!"
+    );
+  }
+
+  #[test]
+  fn test_data_race_multi_threaded() {
+    let runtime: Runtime = Runtime::new().unwrap();
+
+    let mut handles: Vec<JoinHandle<()>> = vec![];
+
+    let total: usize = 100_000;
+
+    for _ in 0..total {
+      let handle: JoinHandle<()> = runtime.spawn(unsafe_add_multi_threaded());
+
+      handles.push(handle);
+    }
+
+    for handle in handles {
+      runtime.block_on(handle).unwrap();
+    }
+
+    assert_ne!(
+      COUNTER_MULTI_THREADED.load(Ordering::SeqCst),
+      total,
+      "race condition did not occur!"
     );
   }
 }
